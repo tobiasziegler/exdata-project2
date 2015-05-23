7ce765c2c4aa2510a205fc30a621a499f1cde418
David Mallard <correspondence@davidmallard.id.au>
2015-05-23 10:55:03 +1000

    Move data files to the main project directory instead of a subdirectory, for consistency with project instructions.

--- get-data.R
+++ get-data.R
@@ -1,11 +1,8 @@
 # Downloads the required data. Run this script before any of the exploratory 
 # analyses.
 dataUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
-lclZip <- file.path("data", "exdata-data-NEI_data.zip")
-if(!file.exists("data")) {
-  dir.create("data")
-}
+lclZip <- "exdata-data-NEI_data.zip"
 if(!file.exists(lclZip)) {
   download.file(dataUrl, lclZip, method = "curl")
 }
-unzip(lclZip, exdir = "data")
+unzip(lclZip)
